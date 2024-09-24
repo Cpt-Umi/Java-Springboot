a1b1e25686a68b5ad6644f3e17eb1af5e6b76546
Muhammad Umair Khan <muhammad.umair@paysyslabs.com>
2024-09-24 16:02:26 +0500

    Ternary Demo

---
+++ Core1/ternary.java
@@ -0,0 +1,12 @@
+class Ternary {
+  public static void main(String args[]) {
+    // Ternary operator exists in Java...
+
+    int number = 256;
+    boolean result;
+
+    result = number % 2 == 0 ? true : false;
+
+    System.out.printf("Result %s", result);
+  }
+}
