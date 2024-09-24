ce7c6be61ce2150a4e0ad9085b705f367ad6f47f
Muhammad Umair Khan <muhammad.umair@paysyslabs.com>
2024-09-24 12:43:06 +0500

    Hello, Friend & Basic data types and operators

---
+++ Sector1/Variables.java
@@ -0,0 +1,14 @@
+class Variables {
+  public static void main(String args[]) {
+    // Char can be incremented
+    char c = 'a';
+    System.out.println(c);
+    c++;
+    System.out.println(c);
+
+    // Type casting
+    int a = 257;
+    byte b = (byte) a;
+    System.out.println(b);
+  }
+}
